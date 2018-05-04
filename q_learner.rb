faf27ba2c935f97ffefc1bb4eee5de99bac20fd2
Andrew Burleson <aburleson@atlassian.com>
2018-05-04 13:53:42 -0700

    Add notes on arguments

--- q_learner.rb
+++ q_learner.rb
@@ -4,6 +4,14 @@ class QLearner
   end
 
   # Setup your initial state here.
+  # Num States: each state in the environment (maze) is represented by a single integer (0..99).
+  # Num Actions: the learner can move in four directions.
+  # Alpha: learning rate
+  # Gamma: discount rate
+  # RAR: initial rate of random action
+  # RADR: rate of decay of random action
+  # Dyna: how many experiences to 'relive' after a query
+  # Verbose: if this is true you can `puts` whatever you want.
   def initialize(num_states: 100, num_actions: 4, alpha: 0.2, gamma: 0.9, rar: 0.5, radr: 0.99, dyna: 0, verbose: false)
   end
 
